108dc01afc03230249796b1c0ae385abb3b3f5fa
Rohit Kumar <rohitkumar@Rohits-MacBook-Air.local>
2025-10-30 18:59:29 +0530

    updated meta pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,18 +56,22 @@ export default function RootLayout({
 
         {/* Meta Pixel base snippet */}
         <Script id="fb-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
-            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '853177857369230'); fbq('track', 'PageView');`}
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1728355057862946');
+fbq('track', 'PageView');`}
         </Script>
 
         {/* No-JS fallback */}
         <noscript>
           <img
-            src="https://www.facebook.com/tr?id=853177857369230&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1728355057862946&ev=PageView&noscript=1"
             alt=""
             height="1"
             width="1"
